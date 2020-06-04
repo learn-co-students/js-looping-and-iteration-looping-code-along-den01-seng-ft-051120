d1aacf229aa09670bf6893d54a1ac2ac4210faf1
WRHR <wrhreeves@gmail.com>
2020-06-03 20:47:05 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function countDown(num) {
+    for (let i = 0; i <= num; i++) {
+        console.log(num - i);
+    };
+};
+
+function writeCards(names, event) {
+    let thankyouCards = [];
+    for (let i = 0; i < names.length; i++) {
+        thankyouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    };
+    return thankyouCards;
+};
